c2138a147e20a847004916b80e737ddb12119a6c
jithendra259 <jithendrasubramanyam218@gmail.com>
2025-01-22 12:46:33 +0530

    FIRST COMMIT

--- air_quality_index/src/components/signup/signup.js
+++ air_quality_index/src/components/signup/signup.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+
 function Signup() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
