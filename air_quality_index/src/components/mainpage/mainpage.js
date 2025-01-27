5a48be48ca4ac3ffbc5593c77b10e91d6b1b58ed
jithendra259 <jithendrasubramanyam218@gmail.com>
2025-01-27 16:20:52 +0530

    FIRST COMMIT

--- air_quality_index/src/components/mainpage/mainpage.js
+++ air_quality_index/src/components/mainpage/mainpage.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { CgProfile } from 'react-icons/cg';
 import './mainpage.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { IoMenuOutline } from "react-icons/io5";
 
 function Mainpage(){
   return (
     <div className="container">
       <div className="mainpageclass">
         <div className="mainheader">
+          <div className='menuicon'>
+            <button type="button" className="btn btn-light" aria-label="Menu">
+            <IoMenuOutline />
+            </button>
+          </div>
           <div className="profileicon">
             <button type="button" className="btn btn-light" aria-label="Profile">
               <CgProfile />
